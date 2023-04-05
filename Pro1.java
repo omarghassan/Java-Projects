312578aa3623489e212306e5bd8e7a735f9d7af8
Omar Ghassan <107811704+omarghassan@users.noreply.github.com>
2023-04-05 05:04:44 +0300

    Add files via upload

---
+++ Pro1.java
@@ -0,0 +1,44 @@
+// Omar Ghassan
+// 202111069
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pro1;
+
+/**
+ *
+ * @author User
+ */
+public class Pro1 {
+
+    /**
+     * @param args the command line arguments
+     */
+   
+    public Pro1() {
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       byte w = 1;
+       int x = 5;
+       long y = 87455;
+       short z = 56;
+       double a = 55.78;
+       float q = 77.6F;
+       char ch1 = 65;
+       
+      System.out.println("w = "+w);
+      System.out.println("x = "+x);
+      System.out.println("y = "+y);
+      System.out.println("z = "+z);
+      System.out.println("a = "+a);
+      System.out.println("q = "+q);
+      System.out.println("ch1 = "+ch1);
+      
+    }
+    
+}
